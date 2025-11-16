924b690dd46797fafb741ec10d7e9e7f6f9a6713
Harshith M <mharshith200@gmail.com>
2025-11-16 20:44:25 +0530

    File upload

--- auth/middleware/auth-middleware.js
+++ auth/middleware/auth-middleware.js
@@ -1,22 +1,32 @@
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+//   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+//   if (!authHeader || !authHeader.startsWith("Bearer ")) {
+//     return res.status(401).json({
+//       success: false,
+//       message: "No token provided",
+//     });
+//   }
+
+//   const token = authHeader.split(" ")[1];
+//   console.log("Token:", token);
+    const token = req.headers.authorization?.split(" ")[1];
+    
+  if (!token) {
     return res.status(401).json({
       success: false,
       message: "No token provided",
     });
   }
 
-  const token = authHeader.split(" ")[1];
   console.log("Token:", token);
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // attach user info
-    next(); // move inside try
+    next(); 
   } catch (error) {
     return res.status(401).json({
       success: false,
---
+++ auth/models/image.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const ImageSchema = new mongoose.Schema(
+  {
+    url: {
+      type: String,
+      required: true,
+    },
+    altText: {
+      type: String,
+      required: false,
+    },
+    publicId: {
+      type: String,
+      required: true,
+    },
+    uploadedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Image", ImageSchema);
